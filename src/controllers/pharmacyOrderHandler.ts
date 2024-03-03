d998cd82a58d60499209883803041982962b33b9
aryan-kanwar <kanwararyan1@gmail.com>
2024-03-03 09:36:12 -0500

    refactor code

--- src/controllers/pharmacyOrderHandler.ts
+++ src/controllers/pharmacyOrderHandler.ts
@@ -5,73 +5,36 @@ export class PharmacyOrderHandler {
     constructor(private pharmacyName: string) {}
 
     createOrder(data: OrderPayload) {
-        switch (this.pharmacyName.toLowerCase()) {
-            case 'healthmart':
-                if ('healthMartProduct' in data && 'healthMartQuantity' in data && 'healthMartCustomerInfo' in data) {
-                    return this.processHealthMartOrder(data);
-                }
-                break;
-            case 'careplus':
-                if ('carePlusProduct' in data && 'carePlusQuantity' in data && 'carePlusClientInfo' in data) {
-                    return this.processCarePlusOrder(data);
-                }
-                break;
-            case 'quickcare':
-                if ('quickCareProduct' in data && 'quickCareQuantity' in data && 'quickCareUserData' in data) {
-                    return this.processQuickCareOrder(data);
-                }
-                break;
-            //can add new pharmacy in case
+        switch (true) {
+            case this.isHealthMartOrder(data):
+                return this.processOrder(data, 'healthMartProduct', 'healthMartQuantity', 'healthMartCustomerInfo');
+            case this.isCarePlusOrder(data):
+                return this.processOrder(data, 'carePlusProduct', 'carePlusQuantity', 'carePlusClientInfo');
+            case this.isQuickCareOrder(data):
+                return this.processOrder(data, 'quickCareProduct', 'quickCareQuantity', 'quickCareUserData');
             default:
                 throw new Error(`Unsupported pharmacy: ${this.pharmacyName}`);
         }
     }
-
-    private processHealthMartOrder(data: HealthMartOrderPayload) {
-        return {
-            pharmacy: this.pharmacyName,
-            product: data.healthMartProduct,
-            quantity: data.healthMartQuantity,
-            customerInfo: {
-                name: data.healthMartCustomerInfo.healthMartCustName,
-                address: data.healthMartCustomerInfo.healthMartCustAddress,
-                city: data.healthMartCustomerInfo.healthMartCustCity,
-                state: data.healthMartCustomerInfo.healthMartCustState,
-                zipcode: data.healthMartCustomerInfo.healthMartCustZipcode,
-                country: data.healthMartCustomerInfo.healthMartCustCountry
-            }
-        };
+    //validation method to test data coming from request
+    private isHealthMartOrder(data: OrderPayload): data is HealthMartOrderPayload {
+        return 'healthMartProduct' in data && 'healthMartQuantity' in data && 'healthMartCustomerInfo' in data;
     }
 
-    private processCarePlusOrder(data: CarePlusOrderPayload) {
-        return {
-            pharmacy: this.pharmacyName,
-            product: data.carePlusProduct,
-            quantity: data.carePlusQuantity,
-            customerInfo: {
-                name: data.carePlusClientInfo.carePlusClientName,
-                address: data.carePlusClientInfo.carePlusClientAddress,
-                city: data.carePlusClientInfo.carePlusClientCity,
-                state: data.carePlusClientInfo.carePlusClientState,
-                zipcode: data.carePlusClientInfo.carePlusClientZipcode,
-                country: data.carePlusClientInfo.carePlusClientCountry
-            }
-        };
+    private isCarePlusOrder(data: OrderPayload): data is CarePlusOrderPayload {
+        return 'carePlusProduct' in data && 'carePlusQuantity' in data && 'carePlusClientInfo' in data;
     }
 
-    private processQuickCareOrder(data: QuickCareOrderPayload) {
+    private isQuickCareOrder(data: OrderPayload): data is QuickCareOrderPayload {
+        return 'quickCareProduct' in data && 'quickCareQuantity' in data && 'quickCareUserData' in data;
+    }
+
+    private processOrder(data: any, productKey: string, quantityKey: string, customerInfoKey: string) {
         return {
             pharmacy: this.pharmacyName,
-            product: data.quickCareProduct,
-            quantity: data.quickCareQuantity,
-            customerInfo: {
-                name: data.quickCareUserData.quickCareUserName,
-                address: data.quickCareUserData.quickCareUserAddress,
-                city: data.quickCareUserData.quickCareUserCity,
-                state: data.quickCareUserData.quickCareUserState,
-                zipcode: data.quickCareUserData.quickCareUserZipcode,
-                country: data.quickCareUserData.quickCareUserCountry
-            }
-        };
+            product: data[productKey],
+            quantity: data[quantityKey],
+            customerInfo: data[customerInfoKey]
+        }
     }
 }
